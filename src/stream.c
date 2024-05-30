fd106c6e718f1604b0994efd5a1e374f58ecad12
Our Air Quality <info@ourairquality.org>
2024-05-30 10:59:35 +1000

    stream ntrip agent: specialize for windows
    To help narrow down issues report by servers.

--- src/stream.c
+++ src/stream.c
@@ -104,7 +104,11 @@
 #define MAXSTATMSG          32          /* max length of status message */
 #define DEFAULT_MEMBUF_SIZE 4096        /* default memory buffer size (bytes) */
 
+#ifdef WIN32
+#define NTRIP_AGENT         "RTKLIB/" VER_RTKLIB "_" PATCH_LEVEL "_Win32"
+#else
 #define NTRIP_AGENT         "RTKLIB/" VER_RTKLIB "_" PATCH_LEVEL
+#endif
 #define NTRIP_CLI_PORT      2101        /* default ntrip-client connection port */
 #define NTRIP_SVR_PORT      80          /* default ntrip-server connection port */
 #define NTRIP_MAXRSP        32768       /* max size of ntrip response */
