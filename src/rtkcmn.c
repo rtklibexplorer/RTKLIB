7c124e051dbd77858b724accc3b6c7642aef7cc2
oaq <info@ourairquality.org>
2024-11-13 00:13:07 +1100

    tropmodel, rtkpos: use the precise mapping function
    Have tropmodel use the precise mapping function, rather than 1/cos(z).
    
    Update the exponent in the pressure vs temperature function, from
    5.2568 to 5.255877 which is found in references on the matter.
    
    Update the rtkpos RTK code to also account for the wet component when
    not estimating it, and to also allow use of the SBAS model when that
    is selected.

--- src/rtkcmn.c
+++ src/rtkcmn.c
@@ -3677,35 +3677,40 @@ extern int seliflc(int optnf,int sys)
     /* use L1/L5 for Galileo if L5 is enabled */
     return((optnf==2||sys!=SYS_GAL)?1:2);
 }
-/* troposphere model -----------------------------------------------------------
-* compute tropospheric delay by standard atmosphere and saastamoinen model
-* args   : gtime_t time     I   time
+
+/* Troposphere model -----------------------------------------------------------
+* Compute tropospheric delay by standard atmosphere and saastamoinen model
+* Args   : gtime_t time     I   time
 *          double *pos      I   receiver position {lat,lon,h} (rad,m)
 *          double *azel     I   azimuth/elevation angle {az,el} (rad)
 *          double humi      I   relative humidity
-* return : tropospheric delay (m)
-*-----------------------------------------------------------------------------*/
-extern double tropmodel(gtime_t time, const double *pos, const double *azel,
-                        double humi)
-{
-    const double temp0=15.0; /* temperature at sea level */
-    double hgt,pres,temp,e,z,trph,trpw;
+* Return : tropospheric delay (m)
+*----------------------------------------------------------------------------*/
+extern double tropmodel(gtime_t time, const double *pos, const double *azel, double humi) {
+  const double temp0 = 15.0; // Temperature at sea level
 
-    if (pos[2]<-100.0||1E4<pos[2]||azel[1]<=0) return 0.0;
+  if (pos[2] < -100.0 || 1E4 < pos[2] || azel[1] <= 0) return 0.0;
 
-    /* standard atmosphere */
-    hgt=pos[2]<0.0?0.0:pos[2];
+  // Standard atmosphere
+  double hgt = pos[2] < 0.0 ? 0.0 : pos[2];
 
-    pres=1013.25*pow(1.0-2.2557E-5*hgt,5.2568);
-    temp=temp0-6.5E-3*hgt+273.16;
-    e=6.108*humi*exp((17.15*temp-4684.0)/(temp-38.45));
+  double temp = temp0 - 6.5E-3 * hgt + 273.16;
+  double pres = 1013.25 * pow(288.15 / temp, -5.255877);
+  double e = 6.108 * humi * exp((17.15 * temp - 4684.0) / (temp - 38.45));
 
-    /* saastamoinen model */
-    z=PI/2.0-azel[1];
-    trph=0.0022768*pres/(1.0-0.00266*cos(2.0*pos[0])-0.00028*hgt/1E3)/cos(z);
-    trpw=0.002277*(1255.0/temp+0.05)*e/cos(z);
-    return trph+trpw;
+  // Saastamoinen model
+  double z = PI / 2.0 - azel[1];
+  double trpzh = 0.0022768 * pres / (1.0 - 0.00266 * cos(2.0 * pos[0]) - 0.00028 * hgt / 1E3);
+  double trpzw = 0.002277 * (1255.0 / temp + 0.05) * e;
+
+  // Fast exit path.
+  if (fabs(z) < 1e-10) return trpzh + trpzw;
+
+  // Precise mapping function
+  double mapfw, mapfh = tropmapf(time, pos, azel, &mapfw);
+  return trpzh * mapfh + trpzw * mapfw;
 }
+
 #ifndef IERS_MODEL
 
 static double interpc(const double coef[], double lat)
