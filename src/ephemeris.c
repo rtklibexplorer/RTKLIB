379f1aca8aeb939d582ab5b961fe19ffa00b7a3f
AndreHauschild <andre.hauschild@gmx.de>
2025-04-30 11:27:44 +0200

    Remove satellite antenna offset correction for SSR streams

--- src/ephemeris.c
+++ src/ephemeris.c
@@ -625,7 +625,7 @@ static int satpos_sbas(gtime_t time, gtime_t teph, int sat, const nav_t *nav,
 }
 /* satellite position and clock with ssr correction --------------------------*/
 static int satpos_ssr(gtime_t time, gtime_t teph, int sat, const nav_t *nav,
-                      int opt, double *rs, double *dts, double *var, int *svh)
+                      double *rs, double *dts, double *var, int *svh)
 {
     const ssr_t *ssr;
     eph_t *eph;
@@ -695,22 +695,6 @@ static int satpos_ssr(gtime_t time, gtime_t teph, int sat, const nav_t *nav,
         /* relativity correction */
         dts[0]-=2.0*dot3(rs,rs+3)/CLIGHT/CLIGHT;
     }
-    /* radial-along-cross directions in ecef */
-    if (!normv3(rs+3,ea)) return 0;
-    cross3(rs,rs+3,rc);
-    if (!normv3(rc,ec)) {
-        *svh=-1;
-        return 0;
-    }
-    cross3(ea,ec,er);
-
-    /* satellite antenna offset correction */
-    if (opt) {
-        satantoff(time,rs,sat,nav,dant);
-    }
-    for (i=0;i<3;i++) {
-        rs[i]+=-(er[i]*deph[0]+ea[i]*deph[1]+ec[i]*deph[2])+dant[i];
-    }
     /* t_corr = t_sv - (dts(brdc) + dclk(ssr) / CLIGHT) (ref [10] eq.3.12-7) */
     dts[0]+=dclk/CLIGHT;
 
@@ -750,8 +734,8 @@ extern int satpos(gtime_t time, gtime_t teph, int sat, int ephopt,
     switch (ephopt) {
         case EPHOPT_BRDC  : return ephpos     (time,teph,sat,nav,-1,rs,dts,var,svh);
         case EPHOPT_SBAS  : return satpos_sbas(time,teph,sat,nav,   rs,dts,var,svh);
-        case EPHOPT_SSRAPC: return satpos_ssr (time,teph,sat,nav, 0,rs,dts,var,svh);
-        case EPHOPT_SSRCOM: return satpos_ssr (time,teph,sat,nav, 1,rs,dts,var,svh);
+        case EPHOPT_SSRAPC: 
+        case EPHOPT_SSRCOM: return satpos_ssr (time,teph,sat,nav,   rs,dts,var,svh);
         case EPHOPT_PREC  :
             if (!peph2pos(time,sat,nav,rs,dts,var)) break; else return 1;
     }
