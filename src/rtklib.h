62d4677ed8425a4e2748c6d390b500d1afb493fc
rtklibexplorer <tim.everett3@gmail.com>
2026-06-22 12:09:36 -0600

    Update version from 2.5.0 to 2.5.1

--- src/rtklib.h
+++ src/rtklib.h
@@ -78,7 +78,7 @@ extern "C" {
 
 #define VER_RTKLIB  "EX"             /* library version */
 
-#define PATCH_LEVEL "2.5.0"               /* patch level */
+#define PATCH_LEVEL "2.5.1"               /* patch level */
 
 #define COPYRIGHT_RTKLIB \
             "Copyright (C) 2007-2020 T.Takasu\nAll rights reserved."
